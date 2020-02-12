307e228b6a6077e119cc156e31ef38e5971d2f8e
Barbara <bab38prapant@hotmail.fr>
2020-02-12 10:42:20 +0100

    TDD4 : test failure

--- tests/Entity/UserTest.php
+++ tests/Entity/UserTest.php
@@ -11,19 +11,17 @@ class UserTest extends TestCase
         $this->user = new User();
     }
     public function testNewUser() {
-        $user = new User();
-        $this->assertInstanceOf(User::class, $user);
+        $this->assertInstanceOf(User::class, $this->user);
         $this->assertNull($this->user->getId());
     }
 
     public function testUserNom() {
-        $user = new User();
-        $user->setNom('Martin');
+        $this->user->setNom('Martin');
         $this->assertEquals("Martin", $this->user->getNom());
     }
 
     public function testUserPrenom(){
-        $this->user->setPrenom();
+        $this->user->setPrenom("Jean-Pierre");
         $this->assertEquals("Jean-Pierre", $this->user->getPrenom());
     }
 }
