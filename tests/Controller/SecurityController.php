3d96427eb236daa4b72fb6a6cbbefb21195324d8
Florent <florent84@live.fr>
2020-02-12 14:20:57 +0100

    Correction fichier txt -> php

--- tests/Controller/SecurityController.php
+++ tests/Controller/SecurityController.php
@@ -1,7 +1,6 @@
 <?php
 namespace App\Tests\Controller;
 
-// use PHPUnit\Framework\TestCase;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class SecurityControllerTest extends WebTestCase {
