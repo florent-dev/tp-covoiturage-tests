6b2892c41b9b9285f6788f7a38f2614fef025904
Barbara <bab38prapant@hotmail.fr>
2020-02-12 10:37:27 +0100

    TDD4 : test user prenom

--- tests/Entity/UserTest.php
+++ tests/Entity/UserTest.php
@@ -5,15 +5,25 @@ use PHPUnit\Framework\TestCase;
 
 class UserTest extends TestCase
 {
+    protected $user;
+
+    public function setUp(){
+        $this->user = new User();
+    }
     public function testNewUser() {
         $user = new User();
         $this->assertInstanceOf(User::class, $user);
-        $this->assertNull($user->getId());
+        $this->assertNull($this->user->getId());
     }
 
     public function testUserNom() {
         $user = new User();
         $user->setNom('Martin');
-        $this->assertEquals("Martin", $user->getNom());
+        $this->assertEquals("Martin", $this->user->getNom());
+    }
+
+    public function testUserPrenom(){
+        $this->user->setPrenom();
+        $this->assertEquals("Jean-Pierre", $this->user->getPrenom());
     }
 }
