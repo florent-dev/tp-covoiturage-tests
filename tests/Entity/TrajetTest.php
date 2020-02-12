f15b24dd43e3c58560e8ceeeb24694bf6ee528fa
Florent <florent84@live.fr>
2020-02-12 11:33:06 +0100

    TDD12 : test - trajet new instance

---
+++ tests/Entity/TrajetTest.php
@@ -0,0 +1,18 @@
+﻿<?php
+
+use PHPUnit\Framework\TestCase;
+
+class TrajetTest extends TestCase
+{
+    protected $trajet;
+
+    public function setUp(): void {
+        $this->trajet = new Trajet();
+    }
+
+    public function testNewTrajet() {
+        $this->assertInstanceOf(Trajet::class, $this->trajet);
+        $this->assertNull($this->trajet->getId());
+    }
+
+}
