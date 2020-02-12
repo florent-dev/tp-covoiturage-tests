629cd8bab6e517884eec21c072964cbe77d578e3
Barbara <bab38prapant@hotmail.fr>
2020-02-12 11:03:24 +0100

    TDD9 : code lieu nom

--- src/Entity/Lieu.php
+++ src/Entity/Lieu.php
@@ -16,8 +16,29 @@ class Lieu
      */
     private $id;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    /**
+     * @param mixed $nom
+     */
+    public function setNom($nom): void
+    {
+        $this->nom = $nom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
     }
 }
