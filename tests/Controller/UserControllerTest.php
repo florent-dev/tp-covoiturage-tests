2ba608d8b15dff79d376d5b7e4a837578379588c
Barbara <bab38prapant@hotmail.fr>
2020-04-01 22:31:50 +0200

    Demande review Florent

---
+++ tests/Controller/UserControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App\Tests\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class UserControllerTest extends WebTestCase
+{
+    public function testInscription()
+    {
+        $client = self::createClient();
+        $client->request('GET', '/inscription');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+}
