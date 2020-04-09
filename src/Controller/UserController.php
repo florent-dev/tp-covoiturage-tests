e4e35233dd9bfacb7a03ada14ced1a04acad7694
Florent <florent84@live.fr>
2020-04-09 19:54:13 +0200

    TDD inscription : code

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,10 +2,18 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
-
+    /**
+     * @Route("/inscription", name="inscription")
+     * @return Response
+     */
+    public function inscription()
+    {
+        return $this->render('user/inscription.html.twig');
+    }
 }
