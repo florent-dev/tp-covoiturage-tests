e662cea13f500b8a6a38d4d1a84ccdd1dcbbe343
Florent <florent84@live.fr>
2020-04-10 11:37:24 +0200

    TDD creation utilisateur : test

--- tests/Controller/UserControllerTest.php
+++ tests/Controller/UserControllerTest.php
@@ -2,6 +2,8 @@
 
 namespace App\Tests\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class UserControllerTest extends WebTestCase
@@ -11,5 +13,32 @@ class UserControllerTest extends WebTestCase
         $client = self::createClient();
         $client->request('GET', '/inscription');
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    public function testInscriptionForm()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/inscription');
+
+        $form = $crawler->selectButton("S'inscrire")->form();
+        $form['registration_form[username]'] = 'UserTest';
+        $form['registration_form[nom]'] = 'TestNom';
+        $form['registration_form[prenom]'] = 'TestPrenom';
+        $form['registration_form[email]'] = 'test@test.com';
+        $form['registration_form[password]'] = 'test';
+        $crawler = $client->submit($form);
+
+        /** @var EntityManagerInterface $em */
+        $em = $this->bootKernel()->getContainer()->get('doctrine')->getManager();
+
+        /** @var User $user */
+        $user = $em->getRepository(User::class)->findOneBy(['username' => 'UserTest']);
+
+        // On vérifie que notre user a bien été sauvegardé.
+        $this->assertSame('TestPrenom', $user->getPrenom());
+
+        // Notre page nous retourne le succès de l'opération.
+        $this->assertSelectorExists('.flash-success');
     }
 }
