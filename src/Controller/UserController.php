694558fcaf9e8ea245a74d5e144207ecbd8341b0
Florent <florent84@live.fr>
2020-04-10 11:37:51 +0200

    TDD creation utilisateur : code

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserController extends AbstractController
 {
@@ -12,8 +16,24 @@ class UserController extends AbstractController
      * @Route("/inscription", name="inscription")
      * @return Response
      */
-    public function inscription()
+    public function inscription(Request $request, UserPasswordEncoderInterface $encoder)
     {
-        return $this->render('user/inscription.html.twig');
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword($encoder->encodePassword($user, $form->get('password')->getData()));
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash('success', ('Votre compte' . $user->getUsername() . ' a été créé !'));
+        }
+
+        return $this->render('user/inscription.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
