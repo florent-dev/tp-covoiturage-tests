bcf3dc8e3a0f3c05748c5a320bbcefc638b7bf10
Florent <florent84@live.fr>
2020-04-10 12:57:38 +0200

    Passage de setUpBeforeClass et tearDownAfterClass dans SecurityControllerTest, ça sera utile pour tester la connexion et déconnexion d'un user

--- tests/Controller/SecurityControllerTest.php
+++ tests/Controller/SecurityControllerTest.php
@@ -1,14 +1,42 @@
 <?php
 namespace App\Tests\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class SecurityControllerTest extends WebTestCase {
+class SecurityControllerTest extends WebTestCase
+{
+    /** @var User $user */
+    static $user;
 
-    public function testLogin() {
-        $client = static::createClient();
-        $client->request('GET', '/login');
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    protected $client;
+
+    public static function setUpBeforeClass(): void
+    {
+        parent::setUpBeforeClass();
+        /** @var EntityManagerInterface $em */
+        $em = self::bootKernel()->getContainer()->get('doctrine')->getManager();
+        self::$user = new User();
+        self::$user->setEmail('jean.dupont@test.com');
+        self::$user->setNom('Dupont');
+        self::$user->setPrenom('Jean');
+        self::$user->setPassword('mdp');
+        self::$user->setUsername('jdupont');
+        $em->persist(self::$user);
+        $em->flush();
+        self::ensureKernelShutdown();
+    }
+
+    protected function setUp(): void
+    {
+        $this->client = self::createClient();
+    }
+
+    public function testLogin()
+    {
+        $this->client->request('GET', '/login');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
     }
 
     // Le createclient ne fonctionne pas, en manuel il y a la redirection, en test rien du tout.
@@ -24,11 +52,22 @@ class SecurityControllerTest extends WebTestCase {
         $this->assertResponseRedirects('/');
     } */
 
-    public function testLogout() {
-        $client = static::createClient();
-        $client->followRedirects();
-        $client->request('GET', '/logout');
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    public function testLogout()
+    {
+        $this->client->followRedirects();
+        $this->client->request('GET', '/logout');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+    }
+
+    public static function tearDownAfterClass(): void
+    {
+        parent::tearDownAfterClass();
+        /** @var EntityManagerInterface $em */
+        $em = self::bootKernel()->getContainer()->get('doctrine')->getManager();
+        $users = $em->getRepository(User::class)->findBy(['email' => 'jean.dupont@test.com']);
+        foreach ($users as $user) $em->remove($user);
+        $em->flush();
+        $em->close();
     }
 
 }
--- tests/Controller/UserControllerTest.php
+++ tests/Controller/UserControllerTest.php
@@ -8,29 +8,11 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class UserControllerTest extends WebTestCase
 {
-    /** @var User $user */
-    static $user;
-
     protected $client;
-
-    public static function setUpBeforeClass(): void
-    {
-        parent::setUpBeforeClass();
-        /** @var EntityManagerInterface $em */
-        $em = self::bootKernel()->getContainer()->get('doctrine')->getManager();
-        self::$user = new User();
-        self::$user->setEmail('jean.dupont@test.com');
-        self::$user->setNom('Dupont');
-        self::$user->setPrenom('Jean');
-        self::$user->setPassword('mdp');
-        self::$user->setUsername('jdupont');
-        $em->persist(self::$user);
-        $em->flush();
-        self::ensureKernelShutdown();
-    }
 
     protected function setUp(): void
     {
+        $this->ensureKernelShutdown();
         $this->client = self::createClient();
     }
 
@@ -67,16 +49,5 @@ class UserControllerTest extends WebTestCase
         // On supprime notre user test.
         $em->remove($registrationUser);
         $em->flush();
-    }
-
-    public static function tearDownAfterClass(): void
-    {
-        parent::tearDownAfterClass();
-        /** @var EntityManagerInterface $em */
-        $em = self::bootKernel()->getContainer()->get('doctrine')->getManager();
-        $users = $em->getRepository(User::class)->findBy(['email' => 'jean.dupont@test.com']);
-        foreach ($users as $user) $em->remove($user);
-        $em->flush();
-        $em->close();
     }
 }
